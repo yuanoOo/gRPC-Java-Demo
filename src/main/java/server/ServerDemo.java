9f78fc88a0b0c12bb1322275a700efa504259d0c
yuan <1212@121.com>
2023-01-02 21:11:36 +0800

    update: format code

--- src/main/java/server/ServerDemo.java
+++ src/main/java/server/ServerDemo.java
@@ -69,9 +69,14 @@ public class ServerDemo {
         // proto文件中的sayHello服务接口被编译后，在生成的HelloGrpc的抽象内部类HelloImplBase中有一个简单的实现
         // 因此，在server端需要重写这个方法，添加上相应的逻辑
         @Override
-        public void sayHello(HelloMessage.HelloRequest req, StreamObserver<HelloMessage.HelloResponse> responseObserver) {
+        public void sayHello(HelloMessage.HelloRequest req,
+                             StreamObserver<HelloMessage.HelloResponse> responseObserver) {
 
-            HelloMessage.HelloResponse reply = HelloMessage.HelloResponse.newBuilder().setMessage("(server端的sayHello()方法处理结果) Hello," + req.getName()).build();
+            HelloMessage.HelloResponse reply = HelloMessage
+                    .HelloResponse
+                    .newBuilder()
+                    .setMessage("(server端的sayHello()方法处理结果) Hello," + req.getName())
+                    .build();
 
 
             // 调用onNext()方法来通知gRPC框架把reply 从server端 发送回 client端
